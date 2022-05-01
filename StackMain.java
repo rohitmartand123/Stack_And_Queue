1373532fd64e57b704b97f50912cdd35c6fafb9b
rohitmartand123 <rohitmartand274@gmail.com>
2022-05-01 23:10:39 +0530

    Added Peek and Pop Functions

--- StackMain.java
+++ StackMain.java
@@ -2,16 +2,16 @@ package com.bridgelabz.stackandqueue;
 import java.util.Scanner;
 /*
  * @author Rohit
- * Program to stack implementation push operation
+ * Ability to peak and pop from the Stack till it is empty
  */
 public class StackMain {
-	 	public static void main(String[] args) {
-	 		System.out.println("------- Welcome to Stacks and Queues Program ---------");
-			System.out.println();
+	public static void main(String[] args) {
+		System.out.println("------- Welcome to Stacks and Queues Program ---------");
+		System.out.println();
 
-			StackImplementation stackImplementation = new StackImplementation();
-			
-			// Calling operation menu method of StackImplementation
-			stackImplementation.operationsMenu();
+		StackImplementation stackImplementation = new StackImplementation();
+
+		// Calling operation menu method of StackImplementation
+		stackImplementation.operationsMenu();
 	 	}
 }
